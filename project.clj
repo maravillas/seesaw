e01864aec0a71ba4b8b9ba3ab8b775c40d885d88
Matthew Maravillas <maravillas@gmail.com>
2010-04-10 14:22:52 -0400

    Initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject seesaw/seesaw "1.0.0-SNAPSHOT"
+  :description "A library for building Swing applications in Clojure."
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+		 [commons-logging/commons-logging "1.1"]
+		 [spectator/spectator "1.0.0"]]
+  :dev-dependencies [[ant/ant "1.6.5"]
+                     [ant/ant-launcher "1.6.5"]
+                     [leiningen/lein-swank "1.2.0-SNAPSHOT"]])
---
+++ test/seesaw/core_test.clj
@@ -0,0 +1,6 @@
+(ns seesaw.core-test
+  (:use [seesaw.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false))
